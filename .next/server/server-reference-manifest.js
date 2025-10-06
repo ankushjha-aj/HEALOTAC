62b61fe0a3850a23f08c82fa8140486959e9530f
ankushjha-aj <jhaankushcpp@gmail.com>
2025-10-06 11:03:07 +0530

    Animesh

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"k/lbv0Ji6qmcLDKwl+qDoQDcKniqzauTyP0IoONcPfQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
